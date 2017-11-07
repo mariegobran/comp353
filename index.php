e82403cf634e9353249394dd5a622e778737126a
mariegobran <mariegobran@gmail.com>
2017-11-07 11:42:09 -0500

    index file change password

--- index.php
+++ index.php
@@ -61,7 +61,7 @@
                
                <form action = "" method = "post">
                   <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
-                  <label>Password  :</label><input type = "text" name = "password" class = "box" /><br/><br />
+                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
                   <input type = "submit" value = " Submit "/><br />
                </form>
                
