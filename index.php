6f5bd0276e3465a53ec573be48b0027c4d94d7f9
eglen <eglen@live.com>
2017-11-07 12:44:07 -0500

    added styles.css

--- index.php
+++ index.php
@@ -30,24 +30,9 @@
 <html>
    
    <head>
-      <title>Login Page</title>
+   <link rel="stylesheet" href="styles.css">
       
-      <style type = "text/css">
-         body {
-            font-family:Arial, Helvetica, sans-serif;
-            font-size:14px;
-         }
-         
-         label {
-            font-weight:bold;
-            width:100px;
-            font-size:14px;
-         }
-         
-         .box {
-            border:#666666 solid 1px;
-         }
-      </style>
+    <title>Login Page</title>
       
    </head>
    
