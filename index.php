6280eaaed70035a81ae9f634982025915430f61c
mariegobran <mariegobran@gmail.com>
2017-11-07 10:51:28 -0500

    Update index.php

--- index.php
+++ index.php
@@ -61,8 +61,8 @@
                
                <form action = "" method = "post">
                   <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
-                  <label>Password  :</label><input type = "text" name = "password" class = "box" /><br/><br />
-                  <input type = "submit" value = " Submit "/><br />
+                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
+                  <input type = "submit" value = " Submit "/><br />s
                </form>
                
 					
