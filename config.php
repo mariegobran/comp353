ed997242a8ec366166a9a9476687b4bbc2564f9e
Eglen <eglen@live.com>
2017-11-14 16:32:02 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "pvc353_2.encs.concordia.ca";
+$username = "pvc353_2";
+$password = "6yer537e";
+$dbname = "pvc353_2";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+?>
