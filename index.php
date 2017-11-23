b4659b1f286ce5fe41954a1393fc69ed06652555
eglen <eglen@live.com>
2017-11-22 20:36:04 -0500

    sessions

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
-<?php
-   include("config.php");
-   session_start();
+<?php session_start(); ?>
+   
+   <?php include("config.php");
+   
    
    if($_SERVER["REQUEST_METHOD"] == "POST") {
      
