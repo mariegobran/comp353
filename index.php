4fe674e62998d68d41b0944c129cfba4b206020b
eglen <eglen@live.com>
2017-11-28 21:10:42 -0500

    view and edit plan

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
       $myusername = mysqli_real_escape_string($conn,$_POST['username']);
       $mypassword = mysqli_real_escape_string($conn,$_POST['password']);  
       
-      $sql = "SELECT userID, usertype, card FROM users WHERE username = '$myusername' and password = '$mypassword'";
+      $sql = "SELECT * FROM users WHERE username = '$myusername' and password = '$mypassword'";
       $result = mysqli_query($conn,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       
